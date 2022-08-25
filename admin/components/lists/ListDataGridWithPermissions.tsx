f7b2de7be9e5462b1617550b2fd981188dce618a
Demon <F/22nG;c9@zB4%P>
2022-08-25 19:32:52 +0530

    Location Filters

--- admin/components/lists/ListDataGridWithPermissions.tsx
+++ admin/components/lists/ListDataGridWithPermissions.tsx
@@ -1,4 +1,4 @@
-import {Datagrid, List} from 'react-admin';
+import {Datagrid, List, useResourceContext} from 'react-admin';
 import {usePermissions} from "ra-core";
 import {ItemWithPermissionResolver} from "../layout/MenuOptions";
 
@@ -8,7 +8,8 @@ const ListDataGridWithPermissions = ({
                                          dataGridProps
                                      }: { children: any, listProps?: any, dataGridProps?: any }) => {
     const {permissions} = usePermissions();
-    const ResourceWithPermission = ItemWithPermissionResolver(permissions, 'e_samwaad_user')
+    const resource = useResourceContext();
+    const ResourceWithPermission = ItemWithPermissionResolver(permissions, resource)
     const _dataGridProps = dataGridProps ? JSON.parse(JSON.stringify(dataGridProps)) : {};
     if (!ResourceWithPermission?.resourcePermissions?.canDelete) {
         _dataGridProps.bulkActionButtons = null;
