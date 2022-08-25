2d9163085e75f38ba66a30c64d0ec61aa1696ce1
Demon <F/22nG;c9@zB4%P>
2022-08-25 10:20:45 +0530

    Location Filters

--- admin/resources/location/LocationEdit.tsx
+++ admin/resources/location/LocationEdit.tsx
@@ -1,37 +1,37 @@
 import {
-  TextInput,
-  useRecordContext,
-  ReferenceInput,
-  useDataProvider,
-  SelectInput,
-  Edit,
-  SimpleForm,
+    TextInput,
+    useRecordContext,
+    ReferenceInput,
+    useDataProvider,
+    SelectInput,
+    Edit,
+    SimpleForm,
 } from "react-admin";
 
 const LocationForm = () => {
-  const record = useRecordContext();
-  const dataProvider = useDataProvider();
-  console.log(dataProvider);
-  return (
-    <>
-      <span>Location Details</span>
-      <div>
-        <ReferenceInput source="id" reference="location">
-          <SelectInput disabled optionText={"id"} />
-        </ReferenceInput>
-        <TextInput locales="en-IN" source="district" />
-        <TextInput source="block" />
-        <TextInput source="cluster" />
-      </div>
-    </>
-  );
+    const record = useRecordContext();
+    const dataProvider = useDataProvider();
+    console.log(dataProvider);
+    return (
+        <>
+            <span>Location Details</span>
+            <div>
+                <ReferenceInput source="id" reference="location">
+                    <SelectInput disabled optionText={"id"}/>
+                </ReferenceInput>
+                <TextInput source="district"/>
+                <TextInput source="block"/>
+                <TextInput source="cluster"/>
+            </div>
+        </>
+    );
 };
 
 export const LocationEdit = () => (
-  <Edit mutationMode={"pessimistic"}>
-    <SimpleForm>
-      <LocationForm />
-    </SimpleForm>
-  </Edit>
+    <Edit mutationMode={"pessimistic"}>
+        <SimpleForm>
+            <LocationForm/>
+        </SimpleForm>
+    </Edit>
 );
 export default LocationEdit;
