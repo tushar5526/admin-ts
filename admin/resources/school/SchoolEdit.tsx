1ffa561db8401b3d8c288e908b0408db78ca3669
Demon <F/22nG;c9@zB4%P>
2022-08-25 20:08:57 +0530

    Location Filters

--- admin/resources/school/SchoolEdit.tsx
+++ admin/resources/school/SchoolEdit.tsx
@@ -1,45 +1,23 @@
 import {
-    TextInput,
-    useRecordContext,
-    ReferenceInput,
-    useDataProvider,
-    SelectInput,
-    Edit,
-    SimpleForm,
+  TextInput,
+  ReferenceInput,
+  SelectInput,
+  Edit,
+  SimpleForm,
 } from "react-admin";
-import {WithMyBlocks, WithMyClusters, WithMyDistricts} from "../../components/withAccesses";
 
-export const SchoolEdit = () => {
-    return <Edit mutationMode={"optimistic"}>
-        <SimpleForm>
-            <WithMyDistricts>
-                {
-                    (districts: any) => {
-                        return <>Districts: {districts.length}</>
-                    }
-                }
-            </WithMyDistricts>
-            <WithMyBlocks district={"CHAMBA"}>
-                {
-                    (blocks: any) => {
-                        return <>Hello: {blocks.length}</>
-                    }
-                }
-            </WithMyBlocks>
-            <WithMyClusters block={"CHAMBA"}>
-                {
-                    (clusters: any) => {
-                        return <>Hello: {clusters.length}</>
-                    }
-                }
-            </WithMyClusters>
-            {/*<ReferenceInput source="id" reference="location">*/}
-            {/*    <SelectInput disabled optionText={"id"}/>*/}
-            {/*</ReferenceInput>*/}
-            {/*<TextInput source="district"/>*/}
-            {/*<TextInput source="block"/>*/}
-            {/*<TextInput source="cluster"/>*/}
-        </SimpleForm>
-    </Edit>
-};
+export const SchoolEdit = () => (
+  <Edit mutationMode={"pessimistic"}>
+    <SimpleForm>
+        <div>
+            <ReferenceInput source="id" reference="location">
+                <SelectInput disabled optionText={"id"} />
+            </ReferenceInput>
+            <TextInput  source="district" />
+            <TextInput source="block" />
+            <TextInput source="cluster" />
+        </div>
+    </SimpleForm>
+  </Edit>
+);
 export default SchoolEdit;
