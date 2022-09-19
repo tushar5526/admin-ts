b68c4636594cd0593334bcb640df692115be6b24
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2022-09-19 10:42:59 +0530

    made changes

--- admin/resources/user/esamwaad/UserList.tsx
+++ admin/resources/user/esamwaad/UserList.tsx
@@ -1,38 +1,57 @@
-import {BooleanField, Datagrid, List, NumberField, ReferenceField, TextField, FunctionField} from 'react-admin';
-import {ListDataGridWithPermissions} from "../../../components/lists";
+import {
+  BooleanField,
+  Datagrid,
+  List,
+  NumberField,
+  ReferenceField,
+  TextField,
+  FunctionField,
+} from "react-admin";
+import { ListDataGridWithPermissions } from "../../../components/lists";
 
-const ApplicationId = 'f0ddb3f6-091b-45e4-8c0f-889f89d4f5da';
+const ApplicationId = "f0ddb3f6-091b-45e4-8c0f-889f89d4f5da";
 const DisplayRoles = (a: any) => {
-    const registration = a.registrations?.find((r: any) => r.applicationId === ApplicationId);
-    if (!registration) {
-        return <span>-</span>
-    }
-    const {roles} = registration;
-    return roles.map((role: any, index: number) => {
-        return <span style={{
-            border: '1px solid rgba(224, 224, 224, 1)',
-            padding: '5px',
-            marginRight: '5px',
-            marginBottom: '5px'
-        }} key={index}>{role}</span>
-    })
-}
+  const registration = a.registrations?.find(
+    (r: any) => r.applicationId === ApplicationId
+  );
+  if (!registration) {
+    return <span>-</span>;
+  }
+  const { roles } = registration;
+  return roles.map((role: any, index: number) => {
+    return (
+      <span
+        style={{
+          border: "1px solid rgba(224, 224, 224, 1)",
+          padding: "5px",
+          marginRight: "5px",
+          marginBottom: "5px",
+        }}
+        key={index}
+      >
+        {role}
+      </span>
+    );
+  });
+};
 const UserList = () => (
-    <ListDataGridWithPermissions dataGridProps={{rowClick: "edit"}}>
-        <TextField source="username"/>
-        <FunctionField
-            label="Full Name"
-            render={(record: any) => `${record.firstName} ${record.lastName}`}
-        />;
-        <NumberField source="mobilePhone" label="Mobile Phone"/>
-        <FunctionField
-            label="Full Name"
-            render={(record: any) => `${record.firstName} ${record.lastName}`}
-        />;
-        <FunctionField
-            label="Role"
-            render={(record: any) => DisplayRoles(record)}
-        />
-    </ListDataGridWithPermissions>
+  <ListDataGridWithPermissions dataGridProps={{ rowClick: "edit" }}>
+    <TextField source="username" />
+    <FunctionField
+      label="Full Name"
+      render={(record: any) => `${record.firstName} ${record.lastName}`}
+    />
+    ;
+    <NumberField source="mobilePhone" label="Mobile Phone" />
+    <FunctionField
+      label="Full Name"
+      render={(record: any) => `${record.firstName} ${record.lastName}`}
+    />
+    ;
+    <FunctionField
+      label="Role"
+      render={(record: any) => DisplayRoles(record)}
+    />
+  </ListDataGridWithPermissions>
 );
 export default UserList;
