9322051957afb06766e1e0ddf92da0aceedca0d9
Shivansh Rana <shivanshrana789@gmail.com>
2022-09-25 19:56:42 +0530

    fetch udise coulmn and apply filters

--- admin/resources/gradeAssessment/GradeAssessmentList.tsx
+++ admin/resources/gradeAssessment/GradeAssessmentList.tsx
@@ -1,22 +1,76 @@
-import { TextField, ReferenceField, DateField } from "react-admin";
+import {
+  TextField,
+  ReferenceField,
+  DateField,
+  TextInput,
+  useDataProvider,
+} from "react-admin";
 import { ListDataGridWithPermissions } from "../../components/lists";
+import { useQuery } from "react-query";
+import { useMemo, useState } from "react";
+import { useLocation } from "react-router-dom";
+import _ from "lodash";
 
 const GradeAssessmentList = () => {
-  return (
-    <ListDataGridWithPermissions>
-      <TextField source="id" />
-      <ReferenceField source="assessment_id" reference="assessment">
-        <TextField label={"Type"} source="type" />
-      </ReferenceField>
-      <ReferenceField source="school_id" reference="school">
-        <TextField label={"Type"} source="name" />
-      </ReferenceField>
+  const location = useLocation();
+  const params: any = new Proxy(new URLSearchParams(location.search), {
+    get: (searchParams, prop) => searchParams.get(prop as string),
+  });
+  const initialFilters = params.filter ? JSON.parse(params.filter) : null;
 
-      <TextField source="section" />
+  const [selectUdise, setSelectUdise] = useState(initialFilters?.udise || "");
+
+  const dataProvider = useDataProvider();
+
+  const {
+    data: _schoolData,
+    isLoading,
+    error,
+  } = useQuery(["school", "getList", {}], () =>
+    dataProvider.getList("school", {
+      pagination: { perPage: 10000, page: 1 },
+      sort: { field: "id", order: "asc" },
+      filter: {},
+    })
+  );
+
+  const schoolData = useMemo(() => {
+    return _schoolData?.data;
+  }, [_schoolData]);
+  console.log(schoolData, "schoolData");
+
+  const school = useMemo(() => {
+    if (!selectUdise || !schoolData) {
+      return [];
+    }
+    return _.uniqBy(
+      schoolData.filter((d) => d.udise === selectUdise),
+      "udise"
+    ).map((a) => {
+      return {
+        id: a.udise,
+        name: a.udise,
+      };
+    });
+  }, [selectUdise, schoolData]);
+  const Filters = [
+    <TextInput label="School" source="school_id" key={"search"} />,
+    <TextInput label="Grade Number" source="grade_number" key={"search"} />,
+    <TextInput label="Udise" source="udise" key={"search"} />,
+  ];
+  return (
+    <ListDataGridWithPermissions listProps={{ filters: Filters }}>
+      <TextField source="id" />
+      <TextField label={"Assessment"} source="assessment_id" />
       <TextField source="grade_number" />
-      <ReferenceField source="streams_id" reference="stream">
-        <TextField label={"tag"} source="tag" />
+      <TextField source="section" />
+      <TextField source="school_id" />
+
+      <ReferenceField label="Udise" source="school_id" reference="school">
+        <TextField source="udise" />
       </ReferenceField>
+      <TextField source="streams_id" />
+
       <DateField source="created" />
       <DateField source="updated" />
     </ListDataGridWithPermissions>
--- admin/resources/gradeAssessment/index.tsx
+++ admin/resources/gradeAssessment/index.tsx
@@ -1,15 +1,15 @@
 import SchoolIcon from "@mui/icons-material/School";
+import { ListGuesser } from "react-admin";
 import GradeAssessmentList from "./GradeAssessmentList";
-
 
 export default {
   list: GradeAssessmentList,
   // edit: null,
   icon: SchoolIcon,
   permissions: {
-    canEdit: ['Admin'],
-    canDelete: ['Admin'],
-    canCreate: ['Admin'],
-    canList: ['Admin'],
-  }
+    canEdit: ["Admin"],
+    canDelete: ["Admin"],
+    canCreate: ["Admin"],
+    canList: ["Admin"],
+  },
 };
