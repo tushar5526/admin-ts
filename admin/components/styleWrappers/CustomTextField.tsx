1128d395a322e152d6e164958c081d2416be2e15
Harshil Jani <harshiljani2002@gmail.com>
2022-10-18 01:43:39 +0530

    UDISE Verfication

--- admin/components/styleWrappers/CustomTextField.tsx
+++ admin/components/styleWrappers/CustomTextField.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const CustomTextField = ({ i, label, type = "text" }: any) => {
+const CustomTextField = ({ i, label, type = "text", customStyle}: any) => {
   return (
-    <div className="custom_text_field">
+    <div className="custom_text_field" style={{...customStyle}}>
       <p>{label}</p>
       <p>{i}</p>
     </div>
