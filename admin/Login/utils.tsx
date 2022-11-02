12678a0575886486022e954f92997cbcb8fee9d3
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-11-02 17:39:38 +0530

    Revert "Assessment new fixes (#93)" (#94)
    This reverts commit c490248692d42be64b2f2c4cee97c18ec1c0ae14.

--- admin/Login/utils.tsx
+++ admin/Login/utils.tsx
@@ -10,32 +10,9 @@ export const getToken = async (userName: string, password: string) => {
   };
   const response = await client.post("/user/login", body);
   if (response?.data?.result?.data) {
-
-    // Storing orinal setItem prototype.
-    const originalSetItem = localStorage.setItem;
-
-    // Setting the userData in storage before modifying function calls.
     localStorage.setItem(
       "userData",
       JSON.stringify(response?.data?.result?.data)
-    );
-
-    // Creating a custom event based method to override setItem.
-    localStorage.setItem = function (key, value) {
-      const event: any = new Event('userFetched');
-
-      event.value = value;
-      event.key = key;
-
-      document.dispatchEvent(event);
-
-      originalSetItem.apply(this, arguments);
-    };
-
-    // Setting token to be used in prepareDataProviders
-    localStorage.setItem(
-      "jwtToken",
-      response?.data?.result?.data?.user?.token
     );
   }
   return response;
