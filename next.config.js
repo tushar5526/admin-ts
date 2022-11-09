456b699177fedb841664488bd401fe838762086e
tushar5526 <codingid6@gmail.com>
2022-11-09 15:25:45 +0530

    Add reverse proxy for /admin

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? '/admin' : undefined,
 }
 
 module.exports = nextConfig
