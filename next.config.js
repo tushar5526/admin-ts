493ac9f26243c606da58b7a278a51e5656ec9b22
tushar5526 <codingid6@gmail.com>
2022-11-09 15:41:06 +0530

    Added a comment for assetPrefix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // "/admin" is the path we would use in the reverse proxy, so change accordingly if you want to use some other path
   assetPrefix: isProd ? '/admin' : undefined,
 }
 
